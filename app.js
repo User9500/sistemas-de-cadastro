9b30a7cc2795e2a5c95e27774cdef886b1104aab
User9500 <ssmachado.igor@gmail.com>
2023-01-11 14:20:59 -0300

    alteracao nova

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicacao esta se conectando ao banco de dados")
+//mais um console de teste:
+console.log("O paulo fez esse commit sem voce ver na aula")
+console.log("Enviando para o Github")
